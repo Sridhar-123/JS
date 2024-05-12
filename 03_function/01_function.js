86636f120ebd5fd273143794e61c8c83f737e2b0
Sridhar-123 <94796980+Sridhar-123@users.noreply.github.com>
2024-05-12 11:30:16 +0000

    function

---
+++ 03_function/01_function.js
@@ -0,0 +1,24 @@
+function saymyname(){
+    console.log("S");
+    console.log("R");
+    console.log("I");
+    console.log("D");
+    console.log("H");
+    console.log("A");
+    console.log("R");
+
+}
+saymyname();
+
+function addTwoNum(num1,num2){
+    console.log(num1+num2);
+}
+addTwoNum(9);
+
+
+function say() {
+   return 2+4;
+}
+
+const hlo = say();
+console.log(hlo);
